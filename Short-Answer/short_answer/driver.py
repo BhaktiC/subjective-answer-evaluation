06643c5b1335f5cb90c322672adb361bb82070d5
aakash <aakash21696@gmail.com>
2017-12-07 18:01:14 +0530

    Sample pull request

--- Short-Answer/short_answer/driver.py
+++ Short-Answer/short_answer/driver.py
@@ -2,7 +2,7 @@ import knn
 import CosineDistance as cd
 import keyword_score as key
 import lsa_score as lsa
-
+#Sample comment to understand branching in git
 def main(stud_ans):
     keyresult = key.main(stud_ans)
     cdresult = cd.main(stud_ans)
@@ -16,4 +16,3 @@ def main(stud_ans):
 if __name__ == "__main__":
     stud_ans = raw_input("Enter answer: ")
     main(stud_ans)
-
