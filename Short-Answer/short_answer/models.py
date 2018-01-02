18badd4717fb1363230eaa64b6bdab62088e1c89
aakash <aakash21696@gmail.com>
2018-01-02 17:51:20 +0530

    Major database changes, system completely dynamic

--- Short-Answer/short_answer/models.py
+++ Short-Answer/short_answer/models.py
@@ -3,9 +3,6 @@ from django.contrib.auth.models import User
 from django.forms import ModelForm
 from django import forms
 
-
-
-
 class UserProfile(models.Model):
         # ROLE_CHOICES = (
         #    ('Teacher', 'Teacher'),
@@ -18,3 +15,16 @@ class UserProfile(models.Model):
         isStudent = models.BooleanField()
         def __unicode__(self):
                 return self.user.username
+
+class QuestionBank(models.Model):
+        question_text = models.CharField(max_length = 500)
+        def __unicode__(self):
+            return self.question_text
+
+class Test(models.Model):
+        test_code = models.CharField(max_length = 6, unique = True)
+        question_nos = models.CharField(max_length = 20)
+        date_created = models.DateTimeField(auto_now_add = True)
+        created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+        def __unicode__(self):
+            return self.test_code
